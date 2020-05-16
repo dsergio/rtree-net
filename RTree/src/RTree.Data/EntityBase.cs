17ed45cedbe536d401f752f1705a46d88b13ead8
David Sergio <davidsergio52@gmail.com>
2020-05-16 13:54:52 -0700

    updates

---
+++ RTree/src/RTree.Data/EntityBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RTree
+{
+	public class EntityBase
+	{
+		[Required]
+		public int Id { get; protected set; }
+	}
+}
---
+++ RTree/src/RTree.Data/LocationItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RTree.Data
+{
+	public class LocationItem : FingerPrintEntityBase
+	{
+		private int X { get; set; }
+		private int Y { get; set; }
+		public string Type { get => _Type; set => _Type = value ?? throw new ArgumentNullException(nameof(Type)); }
+		private string _Type = string.Empty;
+
+		public LocationItem(int x, int y, string type)
+		{
+			X = x;
+			Y = y;
+			Type = type;
+		}
+	}
+}
